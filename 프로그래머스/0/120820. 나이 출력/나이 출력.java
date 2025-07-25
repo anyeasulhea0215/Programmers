d5aabbf02d3e15625f204cdb77d6b82bcb294392
anyeasulhea0215 <144551551+anyeasulhea0215@users.noreply.github.com>
2025-07-25 10:02:53 +0900

    [level 0] Title: 나이 출력, Time: 0.02 ms, Memory: 100 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public static int solution(int age) {
+        int answer = 0;
+        
+        if(age>0&&age<=120){
+            answer=2022-age+1;
+        }
+        
+        return answer;
+        
+    }
+}
