34a3b890df608f41e2f9b6d75bf3018c3f433467
anyeasulhea0215 <144551551+anyeasulhea0215@users.noreply.github.com>
2025-11-28 10:48:40 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+       
+        int a=0;
+        int b=0;
+        
+        for( String g: goal){
+            if(a< cards1.length && cards1[a].equals(g)){
+                a++;
+            }else if(b<cards2.length && cards2[b].equals(g)){
+                b++;
+            }else{
+                return "No";
+            }
+           
+        }
+         return "Yes";
+       
+        /*
+         cards1, cards2 를 조합하여 문자열 배열 생성, 
+         goal의 각 원소와 생성한 문자열 원소을 각각 비교
+          xx  
+          
+          => cards1,cards2의 각 인덱스를 생성하여 인덱스 위치의 원소와 goal인덱스 원소를 비교..          
+        */
+        
+    }
+}
