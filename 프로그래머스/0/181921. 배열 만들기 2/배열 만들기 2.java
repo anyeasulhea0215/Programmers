a1e0f568b35ef4f997b516a5f27308ced407ad97
anyeasulhea0215 <144551551+anyeasulhea0215@users.noreply.github.com>
2025-12-02 13:17:15 +0900

    [level 0] Title: 배열 만들기 2, Time: 15.66 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int l, int r) {
-        int[] answer = {}; // 선언 위치 유지
+        int[] answer = {}; 
 
         List<Integer> tmp = new ArrayList<>();
 
@@ -11,12 +11,13 @@ class Solution {
             //  5의 배수만 후보
             if (i % 5 != 0) continue;
 
-            int num = i / 5;
+            int num = i / 5; // 55->11,  555->111, 50->10 ...
             boolean ok = true;
 
             //  자리수 검사: 0 또는 1로만 구성되어야 함
             while (num > 0) {
                 int digit = num % 10;
+                
                 if (digit != 0 && digit != 1) {
                     ok = false;
                     break;
@@ -28,7 +29,7 @@ class Solution {
             if (ok) tmp.add(i);
         }
 
-        //  결과가 없다면 [-1] 반환
+        //  결과가 없다면 [-1] 
         if (tmp.isEmpty()) {
             answer = new int[]{ -1 };
             return answer;
@@ -37,7 +38,7 @@ class Solution {
         //  answer 배열로 옮기기 
         answer = new int[tmp.size()];
         for (int i = 0; i < tmp.size(); i++) {
-            answer[i] = tmp.get(i);
+            answer[i] = tmp.get(i); // answer배열에 tmp원소 저장
         }
 
         return answer;
